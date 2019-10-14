06655e13a063cef4b7164c9e9c443ed7d533c164
luciano1997 <lucianogarridosepulveda@gmail.com>
2019-10-14 11:41:32 -0300

    Primer Commit

---
+++ AmbienteVirtual/ambienteProgramando/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\proyectoprogramando\ambientevirtual\ambienteprogramando\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
