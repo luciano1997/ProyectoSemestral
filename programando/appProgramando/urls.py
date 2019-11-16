6ab13af4e034980dfef4ab75810489ea824c8a2a
luciano1997 <lucianogarridosepulveda@gmail.com>
2019-11-16 12:40:36 -0300

    todo funcionando

--- programando/appProgramando/urls.py
+++ programando/appProgramando/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -35,3 +36,5 @@ urlpatterns = [
     
     
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
