9444c0d07995e108b54c048c7220d422e7cc28ac
luciano1997 <lucianogarridosepulveda@gmail.com>
2019-11-25 13:40:24 -0300

    arreglar contraseña mail para que funcione

--- programando/programando/settings.py
+++ programando/programando/settings.py
@@ -143,5 +143,5 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'lucianogarridosepulveda@gmail.com'
-EMAIL_HOST_PASSWORD ='Enseriopayaso1'
+EMAIL_HOST_PASSWORD ='1'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
